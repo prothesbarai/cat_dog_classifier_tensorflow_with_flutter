c175b294286ef9296fe0ad7023e341ad24cd640d
prothesbarai <aspprothesbarai1619@gmail.com>
2025-10-28 11:08:14 +0600

    _angkan_prothes_shreyasi_

---
+++ android/app/src/main/kotlin/com/prothes/cat_dog_classifier_tensorflow_with_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.prothes.cat_dog_classifier_tensorflow_with_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:cat_dog_classifier_tensorflow_with_flutter/pages/home_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),),
+      home: const HomePage(),
+    );
+  }
+}
